75e33616a3bc976d34c76647d297f35b4f4e1e5b
Prasanna Venkatesan <mail.prasanna.v@gmail.com>
2021-01-25 00:35:35 +0530

    add tailwind 2.0

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,19 @@
 <script lang="ts">
+	import Tailwindcss from './Tailwindcss.svelte';
 	export let name: string;
 </script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<Tailwindcss />
+<main class="p-4 mx-auto text-center max-w-xl">
+	<h1 class="text-6xl leading-normal text-svelte">Hello {name}!</h1>
+	<p class="custom-style">
+		Visit the
+		<a href="https://svelte.dev/tutorial" class="text-blue-500 underline">Svelte tutorial</a>
+		to learn how to build Svelte apps.
+	</p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.custom-style {
+		@apply italic;
 	}
 </style>
